a832dc8bde1d9891691322f81eed8dba40a583f4
Thay <thayanecarvalho3@gmail.com>
2023-12-18 17:17:01 -0300

    Calculadora de Partidas Rankeadas

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const prompt = require("prompt-sync")();
 
-const heroiName = prompt("Digite o nome do herói: ");
-const XP = parseInt(prompt("Digite a quantidade de XP: "));
-let level = "";
+const value = parseInt(prompt("Digite a quantidade de herois para calcular: "));
 
-if (XP < 1000) {
-  level = "Ferro";
-} else if (XP >= 1000 && XP < 2000) {
-  level = "Bronze";
-} else if (XP >= 2000 && XP < 5000) {
-  level = "Prata";
-} else if (XP >= 5000 && XP < 7000) {
-  level = "Ouro";
-} else if (XP >= 7000 && XP < 8000) {
-  level = "Platina";
-} else if (XP >= 8000 && XP < 9000) {
-  level = "Ascendente";
-} else if (XP >= 9000 && XP < 10000) {
-  level = "Imortal";
-} else {
-  level = "Radiante";
+for (let i = 0; i < value; i++){
+  console.log();
+  const wins = parseInt(prompt("Digite a quantidade de vitórias: "));
+  const losses = parseInt(prompt("Digite a quantidade de derrotas: "));
+  
+  calculateRankLevel(wins, losses);
+  
+  function calculateRankLevel(wins, losses) {
+    const winLossBalance = wins - losses;
+    let level = '';
+  
+    if (wins < 10) {
+      level = "Ferro";
+    } else if (wins >= 11 && wins <= 20) {
+      level = "Bronze";
+    } else if (wins >= 21 && wins <= 50) {
+      level = "Prata";
+    } else if (wins >= 51 && wins <= 80) {
+      level = "Ouro";
+    } else if (wins >= 81 && wins <= 90) {
+      level = "Diamante";
+    } else if (wins >= 91 && wins <= 100) {
+      level = "Lendário";
+    } else {
+      level = "Imortal";
+    }
+  
+    console.log(`O Herói tem saldo de ${winLossBalance} e está no nível de ${level}`);
+  }
 }
 
-console.log(`O Herói de nome **${heroiName}** está no nível de **${level}**`);
+
